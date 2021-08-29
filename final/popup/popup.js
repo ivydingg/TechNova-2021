84f68d9aeb7248686b33dbb53528c8cfc77d9584
n34wu <n34wu@uwaterloo.ca>
2021-08-29 03:03:09 -0400

    dummy items with links

--- final/popup/popup.js
+++ final/popup/popup.js
@@ -1,7 +1,22 @@
 window.onload = function () {
+  //slideshow vals
   const SECHAND = 0, NEW = 1;
   var className = ["mySlides_secHand", "mySlides_new"];
   var slideIndex = [1, 1];
+  
+  //hardcoded dummy slideshow
+  var usedNames = ["Adidas | Zip Up", "Grey Adidas Tshirt", "ADIDAS | Mens Ultimate Tee"];
+  var usedUrls = ["https://poshmark.ca/listing/Adidas-Zip-Up-60c252337ec30cead433e3cf", 
+                  "https://poshmark.ca/listing/Grey-Adidas-Tshirt-6125733d6e2846a0697dbf4f", 
+                  "https://poshmark.ca/listing/ADIDAS-Mens-Ultimate-Tee-5e5c9426de696a1dd83f1fde"];
+  var usedPrices = ["$17.19", "$26.99", "$26.99"];
+  var newNames = ["Openly Gray T-Shirt", "Dinosaur Shirt, Pocket Shirt", "Girl Shirt"];
+  var newUrls = ["https://www.etsy.com/ca/listing/857384253/girls-shirt-inspired-shirt?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=grey+shirt&ref=sr_gallery-1-7&organic_search_click=1&pro=1&frs=1", 
+                  "https://www.etsy.com/ca/listing/734984538/openly-gray-t-shirt-color-options?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=grey+shirt&ref=sc_gallery-1-3&plkey=615bbcfe72ad0dea90fa26d54ba2f547343b487f%3A734984538", 
+                  "https://www.etsy.com/ca/listing/1019180965/dinosaur-shirt-pocket-shirt-jurassic?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=grey+shirt&ref=sr_gallery-1-4&organic_search_click=1&pro=1&frs=1"];
+  var newPrices = ["$26.00", "$25.00", "$20.00"];
+
+
   showDivs(slideIndex[SECHAND], SECHAND);
   showDivs(slideIndex[NEW], NEW);
 
@@ -18,6 +33,26 @@ window.onload = function () {
       x[i].style.display = "none";  
     }
     x[slideIndex[div]-1].style.display = "block";  
+    updateItemInfo(div);
+  }
+
+  function updateItemInfo(div){
+    if (div == SECHAND){
+      var itemName = document.getElementById('used-name');
+      itemName.innerHTML = usedNames[slideIndex[div]-1];
+      var itemPrice = document.getElementById('used-price');
+      itemPrice.innerHTML = "Buy at " + usedPrices[slideIndex[div]-1];
+
+      itemPrice.setAttribute('href', usedUrls[slideIndex[div]-1]);
+    }
+    else {
+      var itemName = document.getElementById('new-name');
+      itemName.innerHTML = newNames[slideIndex[div]-1];
+      var itemPrice = document.getElementById('new-price');
+      itemPrice.innerHTML = "Buy at " + newPrices[slideIndex[div]-1];
+
+      itemPrice.href = newUrls[slideIndex[div]-1];
+    }
   }
 
   function updateStars(description) {
@@ -45,7 +80,7 @@ window.onload = function () {
     rateDesc.innerHTML = description;
   }  
 
-  updateStars("Not Good Enough");
+  updateStars("Good");
   document.getElementById("prev_sechand").addEventListener("click", function() { plusDivs(-1, SECHAND) }, false);
   document.getElementById("next_sechand").addEventListener("click", function() { plusDivs(1, SECHAND) }, false);
   document.getElementById("prev_new").addEventListener("click", function() { plusDivs(-1, NEW) }, false);
