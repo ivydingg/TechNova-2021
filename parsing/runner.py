4595f420d5eafcf71bf4cf6c31f37fc26dd0e60f
Tammy Zeng <tammy.zeng908@gmail.com>
2021-08-28 21:31:39 -0400

    renamed brand tags files

--- parsing/runner.py
+++ parsing/runner.py
@@ -4,27 +4,17 @@ def get_brand_list_test(g):
     brands = g.get_some_brand_tags(g.get_categories_test(), True)
     return brands
 
-def get_brand_list_total(g):
-    brands = g.get_every_brand_tag(g.get_categories())
-    return brands
-
-def write_to_file_test(n, brands):
-    f = open('brand_names.txt', 'w')
-    brands_list = brands[:n]
-    for brand in brands_list:
-        f.write(brand + "\n")
-    f.close()
-
 def write_to_file(brands):
-    f = open('brand_names.txt', 'w')
+    f = open('brand_tags.txt', 'w')
     for brand in brands:
         f.write(brand + "\n")
     f.close()
 
 def main():
     g = GoodOnYou()
-    brands = get_brand_list_test(g)
-    write_to_file(brands)
+    # brands_tags = get_brand_list_test(g)
+    # write_to_file(g.get_data(brands_tags))
+
     print("done!")
 
 main()
